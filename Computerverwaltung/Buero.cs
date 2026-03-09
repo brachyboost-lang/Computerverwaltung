310f26bfbd5d741aaa78de2992e7bf2b75337b63
Alexander Lang <Alexander.Lang@it-lang.de>
2026-03-09 12:39:26 +0100

    Add project files.

---
+++ Computerverwaltung/Buero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Computerverwaltung
+{
+    internal class OfficeWorkstation : Computer
+    {
+        int CpuPower { get; set; }
+        int RamSize { get; set; }
+        int HarddriveSize { get; set; }
+
+        public OfficeWorkstation(string name, string IP, int cpuPower, int ramSize, int harddriveSize) : base(name, IP)
+        {
+            this.CpuPower = cpuPower;
+            this.RamSize = ramSize;
+            this.HarddriveSize = harddriveSize;
+        }
+    }
+
+    internal class GraphicWorkstation : OfficeWorkstation
+            {
+        string GpuName { get; set; }
+        int MonitorSize { get; set; }
+
+        public GraphicWorkstation(string name, string IP, int cpuPower, int ramSize, int harddriveSize, string gpuName, int monitorSize) : base(name, IP, cpuPower, ramSize, harddriveSize)
+        {
+            this.GpuName = gpuName;
+            this.MonitorSize = monitorSize;
+        }
+    }
+}
---
+++ Computerverwaltung/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Computerverwaltung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mal gucken ob es schlau war so viele strings zu nutzen?");
+            Console.WriteLine("Hab doch wieder in int umgewandelt in Buero.cs");
+        }
+    }
+
+    public class Computer
+    {
+        public string Name { get; set; }
+        public string IP { get; set; }
+        
+        public Computer(string name, string IP) 
+        {
+            this.Name = name;
+            this.IP = IP;
+        }
+    }
+    public class Terminal
+    {
+        string ConnectedServer { get; set; }
+        string Location { get; set; }
+    }
+    public class Server
+    {
+        int CpuAmount { get; set; }
+        int RamSize { get; set; }
+        int HarddriveAmount { get; set; }
+        int HarddriveSize { get; set; }
+        string Servertype { get; set; }
+        string NetworkSpeed { get; set; }
+    }
+}
